1260cd803c0bb0026057f7e2141064ee2380dd4d
ellie <elliebgdnv@gmail.com>
2024-04-26 19:24:50 +0300

    basic board functionality add

--- src/board.cpp
+++ src/board.cpp
@@ -1,1 +1,26 @@
 #include "board.h"
+#include <algorithm>
+#include <iostream>
+
+board::board(size_t rows, size_t columns) {
+    scene.reserve(rows * columns);
+    for (size_t i = 0; i < rows; ++i) {
+        for (size_t j = 0; j < columns; ++j) {
+            scene.at(i * columns + j) = point(i, j);
+        }
+    }
+}
+
+void board::add_entity(const entity *e) {
+    if (e == nullptr) {
+        std::cout << "entity is nullptr \n";
+        return;
+    }
+
+    if (std::find(scene.begin(), scene.end(), e) != scene.end()) {
+        std::cout << "point: " << e->get_location() << " is taken.\n";
+        return;
+    }
+
+    entities.emplace_back(e);
+}
--- src/board.h
+++ src/board.h
@@ -1,9 +1,17 @@
 #pragma once
 
 #include "entity.h"
+#include "point.h"
+#include <vector>
 
+using std::size_t;
 class board {
+
 public:
+    // using row major ordering for the matrix
+    std::vector<point> scene;
+    std::vector<entity *> entities;
+
     enum directions {
         UP,
         DOWN,
@@ -11,6 +19,9 @@ public:
         LEFT
     };
 
+    board(size_t rows, size_t colums);
+
+    void add_entity(const entity *e);
     void try_move(directions direction, entity *e);
 
 private:
