2524f7f09578b0a8cf1cc54faec9aacf1b10ec44
ellie <elliebgdnv@gmail.com>
2024-04-26 14:58:44 +0300

    add basic structure to entity class

--- src/entity.cpp
+++ src/entity.cpp
@@ -1,5 +1,33 @@
-//
-// Created by coral-the-beautiful on 20/04/24.
-//
-
 #include "entity.h"
+
+entity::entity(int max_hp, const point &location) : max_hp(max_hp), hit_points(max_hp), location(location) {
+    is_still_alive = true;
+}
+
+int entity::get_hit_points() const {
+    return hit_points;
+}
+
+point entity::get_location() const {
+    return location;
+}
+
+void entity::take_damage(int damage) {
+    hit_points -= damage;
+    if (hit_points <= 0)
+        is_still_alive = false;
+}
+
+void entity::heal(int heal_amount) {
+    if (is_still_alive) {
+        if (hit_points + heal_amount > max_hp) {
+            hit_points = max_hp;
+        } else {
+            hit_points += heal_amount;
+        }
+    }
+}
+
+void entity::set_location(const point &next_location) {
+    location = next_location;
+}
--- src/entity.h
+++ src/entity.h
@@ -1,6 +1,23 @@
 #pragma once
+#include "point.h"
 
 class entity {
 private:
+    int hit_points;
+    point location;
+    bool is_still_alive;
+
 public:
+    const int max_hp;
+    bool is_still_alive;
+
+    entity(int max_hp, const point &location);
+
+    int get_hit_points() const;
+    point get_location() const;
+
+    void take_damage(int damage);
+    void heal(int heal_amount);
+
+    void set_location(const point &next_location);
 };
